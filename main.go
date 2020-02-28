5b29db86e1e5e6fe30f89c6a4852ee68d0ef9b09
Mikkel Gadegaard <mikkel@fama.io>
2020-02-27 16:38:29 -0800

    Functions

--- main.go
+++ main.go
@@ -7,6 +7,15 @@ import (
 
 var testTemplate *template.Template
 
+type User struct {
+	ID    int
+	Email string
+}
+
+type ViewUserData struct {
+	User User
+}
+
 type Widget struct {
 	Name  string
 	Price int
@@ -18,26 +27,63 @@ type ViewData struct {
 }
 
 func main() {
-	var err error
-	testTemplate, err = template.ParseFiles("range.html")
-	if err != nil {
-		panic(err)
-	}
-
-	http.HandleFunc("/", handler)
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/functions", functionHandler)
+	http.HandleFunc("/range", rangeHandler)
 	http.ListenAndServe(":3000", nil)
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
+func functionHandler(w http.ResponseWriter, r *http.Request) {
+	testTemplate, err := template.New("functions.html").Funcs(template.FuncMap{
+		"hasPermission": func(eature string) bool {
+			return false
+		},
+	}).ParseFiles("functions.html")
+	if err != nil {
+		panic(err)
+	}
 	w.Header().Set("Content-Type", "text/html")
 
-	err := testTemplate.Execute(w, ViewData{
+	err = testTemplate.Execute(w, ViewUserData{
+		User: User{
+			ID:    1,
+			Email: "someone@gsnail.com",
+		},
+	})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func rangeHandler(w http.ResponseWriter, r *http.Request) {
+	testTemplate, err := template.ParseFiles("range.html")
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Type", "text/html")
+
+	err = testTemplate.Execute(w, ViewData{
 		Name: "John Smith",
 		Widgets: []Widget{
 			Widget{"BlueWidget", 12},
 			Widget{"Red Widget", 12},
 			Widget{"Green Widget", 12},
 		},
+	})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	testTemplate, err := template.ParseFiles("index.html")
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Type", "text/html")
+
+	err = testTemplate.Execute(w, ViewData{
+		Name: "John Smith",
 	})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
